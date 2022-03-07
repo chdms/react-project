0878604d231d06e803897dc3afaad214a45d707d
chdms <chdms267@naver.com>
2022-03-07 14:55:40 +0900

    react project

---
+++ src/hooks/useOrders.js
@@ -0,0 +1,9 @@
+
+import { useContext } from "react";
+import AppStateContext from "../contexts/AppStateContext";
+
+export default function usePrototypes() {
+  const { orders } = useContext(AppStateContext);
+
+  return orders;
+}
